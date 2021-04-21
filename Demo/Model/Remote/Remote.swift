c35f33093ca8219ee6871e314e52e1a9e32efa05
Lonmee <Lonmee@126.com>
2021-04-21 16:25:35 +0800

    remote AF

--- Demo/Model/Remote/Remote.swift
+++ Demo/Model/Remote/Remote.swift
@@ -6,7 +6,8 @@
 //  Copyright © 2020 Lonmee. All rights reserved.
 //
 
-//import SwiftHTTP
+import Foundation
+import Alamofire
 
 let HOST = "http://192.168.199.245:8181"
 let API_VERSION = "/api/v1"
@@ -16,64 +17,66 @@ enum API: String {
     case ACCOUNT = "/account/"
 }
 
-func accountReq(name: String, completionHandler: ((Any) -> Void)? = nil) -> Void {
-//    HTTP.GET(HOST + API_VERSION + API.USER.rawValue + "?name=" + name) { response in
-//        if let err = response.error {
-//            print("error: \(err.localizedDescription)")
-//            return //also notify app of failure as needed
-//        }
-//        if let handler = completionHandler {
-//            handler(decode([User].self, from: response.text!, defaultValue: []))
-//        }
-//    }
-}
+let userArraySerializer = DecodableResponseSerializer<[User]>()
 
-func httpGET(api: API, id: String?, completionHandler: ((Any) -> Void)? = nil) -> Void {
-//    HTTP.GET(HOST + API_VERSION + api.rawValue + (id ?? "")) { response in
-//        if let err = response.error {
-//            print("error: \(err.localizedDescription)")
-//            return //also notify app of failure as needed
-//        }
-//        if let handler = completionHandler {
-//            handler(decode([User].self, from: response.text!, defaultValue: []))
-//        }
-//    }
+func accountReq(name: String, completionHandler: ((Any) -> Void)? = nil) -> Void {
+    let url = HOST + API_VERSION + API.USER.rawValue + "?name=" + name
+    AF.request(url).response(responseSerializer: userArraySerializer) { response in
+        switch response.result {
+        case .success(let data):
+            completionHandler?(data)
+        case .failure(let error):
+            print("error:\(error)")
+        }
+    }
 }
 
 func httpPOST(api: API, param: [User], completionHandler: ((Any) -> Void)? = nil) -> Void {
-//    HTTP.POST(HOST + API_VERSION + api.rawValue, parameters: ["content": String(data: encode(param), encoding: .utf8)!]) { response in
-//        if let err = response.error {
-//            print("error: \(err.localizedDescription)")
-//            return //also notify app of failure as needed
-//        }
-//        if let handler = completionHandler {
-//            handler(decode([User].self, from: response.text!, defaultValue: []))
-//        }
-//    }
+    let url = HOST + API_VERSION + api.rawValue
+    AF.request(url, method: .post, parameters: param, encoder: JSONParameterEncoder()).response(responseSerializer: userArraySerializer) { response in
+        switch response.result {
+        case .success(let data):
+            completionHandler?(data)
+        case .failure(let error):
+            print("error:\(error)")
+        }
+    }
+}
+
+func httpGET(api: API, id: String?, completionHandler: ((Any) -> Void)? = nil) -> Void {
+    let url = HOST + API_VERSION + api.rawValue + (id ?? "")
+    AF.request(url).response(responseSerializer: userArraySerializer) { response in
+        switch response.result {
+        case .success(let data):
+            completionHandler?(data)
+        case .failure(let error):
+            print("error:\(error)")
+        }
+    }
 }
 
 func httpPatch(api: API, param: [User], completionHandler: ((Any) -> Void)? = nil) -> Void {
-//    HTTP.POST(HOST + API_VERSION + api.rawValue, parameters: ["content": String(data: encode(param), encoding: .utf8)!]) { response in
-//        if let err = response.error {
-//            print("error: \(err.localizedDescription)")
-//            return //also notify app of failure as needed
-//        }
-//        if let handler = completionHandler {
-//            handler(decode([User].self, from: response.text!, defaultValue: []))
-//        }
-//    }
+    let url = HOST + API_VERSION + api.rawValue
+    AF.request(url, method: .patch, parameters: param, encoder: JSONParameterEncoder()).response(responseSerializer: userArraySerializer) { response in
+        switch response.result {
+        case .success(let data):
+            completionHandler?(data)
+        case .failure(let error):
+            print("error:\(error)")
+        }
+    }
 }
 
 func httpDelete(api: API, id: String, completionHandler: ((Any) -> Void)? = nil) -> Void {
-//    HTTP.DELETE(HOST + API_VERSION + api.rawValue + id) { response in
-//        if let err = response.error {
-//            print("error: \(err.localizedDescription)")
-//            return //also notify app of failure as needed
-//        }
-//        if let handler = completionHandler {
-//            handler(decode([User].self, from: response.text!, defaultValue: []))
-//        }
-//    }
+    let url = HOST + API_VERSION + api.rawValue + id
+    AF.request(url, method: .delete).response(responseSerializer: userArraySerializer) { response in
+        switch response.result {
+        case .success(let data):
+            completionHandler?(data)
+        case .failure(let error):
+            print("error:\(error)")
+        }
+    }
 }
 
 //func encode<T>(_ para: T) -> Data where T : Encodable {
@@ -86,15 +89,15 @@ func httpDelete(api: API, id: String, completionHandler: ((Any) -> Void)? = nil)
 //    return Data()
 //}
 
-func decode<T>(_ type: T.Type, from: String, defaultValue: T) -> T where T : Decodable {
-//    do {
-//        let decoder = JSONDecoder()
-//        return try decoder.decode(type.self, from: from.data(using: .utf8)!)
-//    } catch {
-//        print(error)
-//    }
-    return defaultValue
-}
+//func decode<T>(_ type: T.Type, from: String, defaultValue: T) -> T where T : Decodable {
+//        do {
+//            let decoder = JSONDecoder()
+//            return try decoder.decode(type.self, from: from.data(using: .utf8)!)
+//        } catch {
+//            print(error)
+//        }
+//    return defaultValue
+//}
 
 //DispatchQueue.main.asyncAfter(deadline: .now()) {}
 //print(UIDevice.current.identifierForVendor!.uuidString)
